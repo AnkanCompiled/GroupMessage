8dfe72c293181246aa72c4419ba32af7ca22f57d
AnkanCompiled <ankanb560@gmail.com>
2024-09-16 23:42:11 +0530

    patch changes

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
-const loginInfoDiv = document.querySelector(".loginfo_div"); //Sign Up Btn Div
-const loginBox = document.querySelector(".login_overlay");
-const registerBox = document.querySelector(".register_overlay");
+const loginInfoDiv = document.querySelector(".loginfo_div"); //Sign Up Button Div
+const loginBox = document.querySelector(".login_overlay"); //Login Box Overlay
+const registerBox = document.querySelector(".register_overlay"); //Register Box Overlay
+const typeForm = document.querySelector(".type_form");
 
 //check if user mongo id saved in local storage
 if (localStorage.getItem("userInfo")) {
@@ -10,7 +11,11 @@ if (localStorage.getItem("userInfo")) {
 }
 
 //adds user name in navbar
-function addUserName() {}
+function addUserName() {
+  const userName = document.createElement("h1");
+  userName.innerHTML(localStorage.getItem("userInfo")["name"]);
+  loginInfoDiv.append(userName);
+}
 
 //adds sign in and sign up button in navbar
 function addRegisterBtn() {
@@ -20,16 +25,24 @@ function addRegisterBtn() {
   registerBtn.id = "registerBtn";
   loginBtn.innerHTML = "Sign In";
   registerBtn.innerHTML = "Sign Up";
-  console.log(loginBtn.id);
   loginInfoDiv.append(loginBtn, registerBtn);
 }
 
+//sign in button click
 document.getElementById("loginBtn").addEventListener("click", () => {
   loginBox.style.display = "flex";
   registerBox.style.display = "none";
 });
 
+//sign up button click
 document.getElementById("registerBtn").addEventListener("click", () => {
   loginBox.style.display = "none";
   registerBox.style.display = "flex";
+});
+
+//type form submit
+typeForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const text = document.getElementById("typeInput").value;
+  console.log(text);
 });
