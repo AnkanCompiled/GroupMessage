1e5702a242c7fa673b7aecc59843180311d4fe2a
Arif Islam <arifislamm2003@gmail.com>
2024-09-19 23:02:16 +0530

    modification to the signup

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,15 +1,22 @@
 const express = require('express')
 const router =  express.Router()
 const USER = require('../model/Register')
+/* 
+wrote a sign up service where 
+--first the body is checked if no body then a bad request
+--if the user is already in the database then we dont let  him signup
+--/if no a reg user then sign up is done and user Created response is sent
+*/
+
 router.post('/signup',async(req,res)=>{
 try{
-    const {name,email,password} = req.body
+    const body = req.body
     if(!body){res.json({status:"bad request"})}
     //if user is already a signed up user
-    const entry = await USER.find({email})
+    const entry = await USER.find(body.email)
     if(entry) res.json({status:"user already present"})
     
-    //if no a reg user then sogn up is created
+    //if no a reg user then sign up is created
     const newUser = new USER(req.body)
     newUser.save()
     .then(res.status(201).json({status:"User Created "}))
