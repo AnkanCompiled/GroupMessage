acf355e82e6d376b01e37cad5cad851c5b8d463f
Arif Islam <arifislamm2003@gmail.com>
2024-09-19 19:07:02 +0530

    lala

--- server/app.js
+++ server/app.js
@@ -2,12 +2,11 @@ const express =  require('express');
 const port = 3001
 const {mongoUri} = require('./connection/config')
 const{mongodbConnect}  = require("./connection/mongoDb")
-const User = require('./model/Register')
+
 
 const app =  express()
 app.use(express.json())
 mongodbConnect(mongoUri);
-
 
 
 
