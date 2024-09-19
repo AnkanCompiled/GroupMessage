92911e368bd6126cf8be9ed7aaa464f5ba50d484
AnkanCompiled <ankanb560@gmail.com>
2024-09-19 20:09:37 +0530

    register fetch commit

--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
 const loginInfoDiv = document.querySelector(".loginfo_div"); //Sign Up Button Div
 const loginBox = document.querySelector(".login_overlay"); //Login Box Overlay
 const registerBox = document.querySelector(".register_overlay"); //Register Box Overlay
-const typeForm = document.querySelector(".type_form");
+const typeForm = document.querySelector(".type_form"); //message typing form
+const loginForm = document.getElementById("loginForm"); //login form
+const registerForm = document.getElementById("registerForm"); //register form
 
 //check if user mongo id saved in local storage
 if (localStorage.getItem("userInfo")) {
@@ -46,3 +48,67 @@ typeForm.addEventListener("submit", (event) => {
   const text = document.getElementById("typeInput").value;
   console.log(text);
 });
+
+//check if name and password entered in Register
+document.getElementById("nameRegisterId").addEventListener("input", (event) => {
+  registerButtonEnable(event.target.value);
+});
+document
+  .getElementById("passwordRegisterId")
+  .addEventListener("input", (event) => {
+    registerButtonEnable(event.target.value);
+  });
+
+function registerButtonEnable() {
+  if (
+    document.getElementById("nameRegisterId").value &&
+    document.getElementById("passwordRegisterId").value
+  ) {
+    document.getElementById("buttonRegisterId").disabled = false;
+  } else {
+    document.getElementById("buttonRegisterId").disabled = true;
+  }
+}
+
+//register form submit click
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  dataEntry();
+});
+
+//send register email , name , password to mongoDB
+async function dataEntry() {
+  const data = await addtoMongo();
+  if (data.message == "success") {
+    document.getElementById("statusRegisterId").innerHTML =
+      "Register Successfull";
+    setTimeout(() => {
+      window.location.reload();
+    }, 500);
+  } else {
+    document.getElementById("statusRegisterId").innerHTML = "Email Exists";
+  }
+}
+
+//fetch request for post to mongoDB
+async function addtoMongo() {
+  try {
+    const res = await fetch("http://localhost:3001/signup", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        name: document.getElementById("nameRegisterId").value,
+        email: document.getElementById("emailRegisterId").value,
+        password: document.getElementById("passwordRegisterId").value,
+      }),
+    });
+    if (res.ok) {
+      const data = await res.json();
+      return data;
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
--- server/app.js
+++ server/app.js
@@ -1,17 +1,7 @@
-const express =  require('express');
-const port = 3001
+const express = require("express");
+const port = 3001;
 
-
-
-
-
-
-
-
-
-
-
-const app =  express()
-app.listen(port,()=>{
-    console.log(`Server Started in http://localhost:${port}/`)
-})
+const app = express();
+app.listen(port, () => {
+  console.log(`Server Started in http://localhost:${port}/`);
+});
